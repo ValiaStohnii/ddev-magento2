1a18ba591b07ae9a845368c10960cd4740ef3bfc
Valia <ValiaStohnii@github.com>
2022-10-26 15:40:51 +0300

    guess game

--- app/code/Stohnii/SecondFooter/view/frontend/web/js/second-footer.js
+++ app/code/Stohnii/SecondFooter/view/frontend/web/js/second-footer.js
@@ -6,19 +6,20 @@
 
 define(['jquery'],function($){
     'use strict';
-    return function(){           
-            $('.click_me_button').click(function () {
+    return function(){    
+        let numbers = [];
+        let guessNumber = 0;
+        let message = ' ';
+        
+               
+        $('.click_me_button').click(function () {
             $('.game_modal_overlay').removeClass('is-hidden');   
             getRandomCard();
         })
         $('.close_button').click(function(){
-            $('.game_modal_overlay').addClass('is-hidden');
+            clear();
         })
 
-        let numbers = [];
-        let guessNumber = 0;
-        // let message = ' ';
-        
         function getRandomCard(){
             let random = Math.floor(Math.random() * 50) + 1;
             console.log(random);
@@ -33,14 +34,28 @@ define(['jquery'],function($){
                 console.log(numbers[i])
                 if(numbers[i] == guessNumber){
                     console.log("You've got Guess Number!")
+                    message = "You've got Guess Number!";
+                    $('.numbers-el').html(`<li> ${numbers} </li>`);
                 } else if( numbers[i] < guessNumber){
                     console.log("was below")
+                    message = "was below";
                 } else if ( numbers[i] > guessNumber){
                     console.log("was above")
+                    message = "was above";
                 }
-                // $('.message-el').textContent(message)
+                $('.message-el').text(message)
             }
         })
+
+        function clear(){
+            message = ' ';
+            numbers = [];
+            guessNumber = 0;
+            $('.game_modal_overlay').addClass('is-hidden');
+            $('.message-el').text(message);
+            $('.numbers-el').text(numbers);
+            $('.game_modal_input').val(' ')
+        }
         
 
     }
