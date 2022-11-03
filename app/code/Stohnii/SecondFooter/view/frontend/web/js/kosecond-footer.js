4a6c27de3b777c25f2b5c9f78ca55c7b560f75ae
Valia <ValiaStohnii@github.com>
2022-11-03 14:56:50 +0200

    knockout

--- app/code/Stohnii/SecondFooter/view/frontend/web/js/kosecond-footer.js
+++ app/code/Stohnii/SecondFooter/view/frontend/web/js/kosecond-footer.js
@@ -1,25 +1,49 @@
 define(['jquery','ko'],function($, ko){
     'use strict';
     return function(){
-        let numbers = [];
+        let numbers = ko.observableArray();
         let guessNumber = 0;
-        let message = ' ';
+        let message = ko.observable();
 
         function getRandomCard(){
             let random = Math.floor(Math.random() * 50) + 1;
             console.log(random);
             guessNumber = random;
         }
+        function clear(){
+            message(' ');
+            numbers([]);
+            guessNumber = 0;
+            $('.game_modal_overlay').addClass('is-hidden');
+            $('.game_modal_input').val(' ')
+        }
+        function newMessage() {
+            for (let i=0; i<numbers().length; i++) {
+                console.log(numbers)
+                if (numbers()[i] == guessNumber) {
+                       console.log(numbers())
+                    message("You've got Guess Number!");
+                } else if (numbers()[i] < guessNumber) {
+                    message("was below");
+                } else if (numbers()[i] > guessNumber) {
+                    message("was above");
+                }
+            }
+        }
         return{
             clickMeButton: function () {
-                console.log('888');
                 $('.game_modal_overlay').removeClass('is-hidden');
                 getRandomCard();
             },
             closeButton: function () {
-                console.log('555');
-                $('.game_modal_overlay').addClass('is-hidden');
-            }
+                clear();
+            },
+            guessButton: function (){
+                numbers.push($('.game_modal_input').val());
+                newMessage();
+            },
+            numbers: numbers,
+            message: message,
         }
     }
 });
